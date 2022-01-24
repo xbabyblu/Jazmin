4bcfb3a297d45828d6a2730dae742fb0c6a95177
xbabyblu <xlilblu666@gmail.com>
2022-01-24 10:49:23 -0500

    Updates

--- index.ts
+++ index.ts
@@ -3,6 +3,9 @@ import WOKCommands from 'wokcommands';
 import path from 'path';
 import dotenv from 'dotenv';
 dotenv.config();
+import mongoose from 'mongoose';
+
+import testSchema from './test-schema'
 
 const client = new DiscordJS.Client({
   intents: [
@@ -13,14 +16,27 @@ const client = new DiscordJS.Client({
   ],
 });
 
-client.on('ready', () => {
-  console.log('Ready!');
-
+client.on('ready', async () => {
+  /*
+  await mongoose.connect(process.env.MONGO_URI || '', {
+    keepAlive: true,
+  });
+*/
   new WOKCommands(client, {
-      commandsDir: path.join(__dirname, 'commands'),
-      typeScript: true,
-      testServers: ['935066669641003058'],
-      botOwners: ['554152090411466754'],
-  })
-})
+    commandsDir: path.join(__dirname, 'commands'),
+    typeScript: true, // typescript only func
+    testServers: ['935066669641003058'],
+    botOwners: ['554152090411466754'],
+    mongoUri: process.env.MONGO_URI
+    // dbOptions:{
+    // keepAlive: true
+    })
+
+    setTimeout(async () => {
+      await new testSchema({
+        message: 'hello world',
+      }).save()
+    }, 1000)
+});
+
 client.login(process.env.TOKEN);
